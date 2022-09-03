1dfde0b11b52e2ea10d8a3a165aec459d61db147
jihanazza <jihanazzahranovarisha@gmail.com>
2022-09-03 16:02:12 +0700

    tes

--- repo.sh
+++ repo.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
     
 sync () {
+    cd ~
+    time rclone copy znxtproject:rom/$ROM_PROJECT/rom.tar.zst ~ -P
+    time tar -xaf rom.tar.zst
+    time rm -rf rom.tar.zst
     cd ~/rom
     repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
-    #git clone https://github.com/ariffjenong/local_manifest.git --depth=1 -b cherish-12.1 .repo/local_manifests
-    repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j24
-    rm -rf .repo
-    find . -type d -name .git -exec rmdir {} \;
+    git clone https://github.com/ariffjenong/local_manifest.git --depth=1 -b cherish-12.1 .repo/local_manifests
+    repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j24 device/sony/maple_dsds device/sony/yoshino-common kernel/sony/msm8998 vendor/sony/maple_dsds
+    #rm -rf .repo
+    #find . -type d -name .git -exec rmdir {} \;
 }
 
 build () {
@@ -31,14 +35,14 @@ build () {
 compile () {
     sync
     echo "done."
-    #build
+    build
 }
 
 cd ~/rom
 ls -lh
-compile #&
-#sleep 114m
-#kill %1
+compile &
+sleep 114m
+kill %1
 #push_kernel
 #push_device
 #push_yoshino
